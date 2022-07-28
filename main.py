60b92bc152d4ca14d4ed13359376174a945dab0f
Yannick Wack <yannick.wack@kuleuven.be>
2022-07-28 16:20:43 +0200

    Translating circNet from matlab

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 28 15:26:18 2022
+
+@author: u0128847
+"""
+import src.netGenerator as netGenerator
+
+network = netGenerator.circNet()
+
+network.addSegments(1)
